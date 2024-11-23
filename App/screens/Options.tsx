a7235b1bdded68aa85b90c0c8854e11a4b53e88c
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-11-23 22:48:01 +0530

    add icon into the row with size and color styling

--- App/screens/Options.tsx
+++ App/screens/Options.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { SafeAreaView, Text, TouchableOpacity, StyleSheet } from "react-native";
 import colors from "../constants/colors";
+import { Entypo } from "@expo/vector-icons";
 
 const styles = StyleSheet.create({
   row: {
     paddingHorizontal: 20,
     paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   text: {
     fontSize: 18,
@@ -23,18 +27,21 @@ export default () => {
     <SafeAreaView>
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>Themes</Text>
+        <Entypo name="chevron-right" size={20} color={colors.blue} />
       </TouchableOpacity>
 
       <view style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native Basic</Text>
+        <Entypo name="export" size={20} color={colors.blue} />
       </TouchableOpacity>
 
       <view style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React native basic by example</Text>
+        <Entypo name="export" size={20} color={colors.blue} />
       </TouchableOpacity>
     </SafeAreaView>
   );
