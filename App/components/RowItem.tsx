3d5e6c695f2c3922c3ce4849e076b7bcc8a22f1c
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-11-24 10:16:19 +0530

    feat: create custom RowItem and RowSeparator compoents

---
+++ App/components/RowItem.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import colors from "../constants/colors";
+import React from "react";
+
+const styles = StyleSheet.create({
+  row: {
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 18,
+    color: colors.text,
+  },
+  separator: {
+    backgroundColor: colors.border,
+    height: StyleSheet.hairlineWidth,
+    marginLeft: 20,
+  },
+});
+
+type RowItemType = {
+  textName: string;
+  rightIcon: React.ReactNode;
+  onPress: () => void;
+};
+
+export const RowItem = ({ rightIcon, textName, onPress }: RowItemType) => {
+  return (
+    <TouchableOpacity style={styles.row} onPress={onPress}>
+      <Text style={styles.text}>{textName}</Text>
+      {rightIcon}
+    </TouchableOpacity>
+  );
+};
+
+export const RowSeparator = () => <view style={styles.separator} />;
