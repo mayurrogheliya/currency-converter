96a06f287631151ca527d3d4f047ec5762687180
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-11-24 19:24:19 +0530

    add logo into the Home screen with styling

--- App/screens/Home.tsx
+++ App/screens/Home.tsx
@@ -1,10 +1,22 @@
-import { StatusBar, StyleSheet, View } from "react-native";
+import { Dimensions, Image, StatusBar, StyleSheet, View } from "react-native";
 import colors from "../constants/colors";
+import bgImage from "../assets/images/bg.png";
+
+const screen = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.blue,
     flex: 1,
+  },
+  logoContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoSize: {
+    width: screen.width * 0.75,
+    height: screen.height * 0.75,
   },
 });
 
@@ -12,6 +24,9 @@ export default () => {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+      <View style={styles.logoContainer}>
+        <Image source={bgImage} resizeMode="contain" style={styles.logoSize} />
+      </View>
     </View>
   );
 };
