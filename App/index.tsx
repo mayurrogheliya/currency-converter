67c9bbc179149a8c2390ee026ec0779fbcde3966
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-02-02 12:08:13 +0530

    created simple local api

--- App/index.tsx
+++ App/index.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "./config/Navigation";
 import { ConversionContextProvider } from "./util/ConversionContext";
+import { api } from "./util/api";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    api("/latest?base=usd")
+      .then((response) => {
+        console.log("response: ", response);
+      })
+      .catch((error) => {
+        console.error("error: ", error);
+      });
+  }, []);
   return (
     <ConversionContextProvider>
       <Navigation />
